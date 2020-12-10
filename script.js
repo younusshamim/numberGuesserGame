77408b4c1af1edef60613213cfc17387b1cb80f6
Younus Shamim <54435896+younusshamim@users.noreply.github.com>
2020-12-11 05:09:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-# Number-Guesser-Game
+
+// let min = prompt('Write the min number:');
+// let max = prompt('Write the max number:');
+
+let min = 1;
+let max = 10;
+let randomNum = Math.floor(Math.random() * (max - min + 1) ) + min;
+
+for(let i=1; i<=3; i++){
+    let guessNum = prompt(`Guess a number between (${min}-${max}):`);
+
+    if(randomNum > guessNum){
+        alert('Correct answer is greater!');
+
+        if(i==3){
+            alert("You Lose!");
+        }
+    }
+    else if(randomNum < guessNum){
+        alert('Correct answer is smaller!');
+
+        if(i==3){
+            alert("You Lose!");
+        }
+    }
+    else if(randomNum == guessNum){
+        alert('You Win');
+        break;
+    }
+    else{
+        alert('Invalid Input');
+    }
+}
